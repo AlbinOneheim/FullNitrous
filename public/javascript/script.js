8f7f5ccbf984c124c4badcd730034e70f974c6da
FullNitrous <fullnitrous@gmail.com>
2018-10-30 14:43:51 +0100

    stuff

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,7 +2,7 @@ function setupCollapse()
 {
   var toggleCollapse = function()
   {
-    var content = this.parentNode.parentNode.querySelector(".content");
+    var content = this.parentNode.querySelector(".content");
     if(content.classList.contains("hidden"))
     {
       content.classList.remove("hidden");
@@ -13,7 +13,7 @@ function setupCollapse()
     }
   }
 
-  var elements = document.querySelectorAll(".collapse .face span");
+  var elements = document.querySelectorAll(".collapse .face");
   for(var i = 0; i < elements.length; i++)
   {
     elements[i].addEventListener("click", toggleCollapse, false);
@@ -96,7 +96,6 @@ function probabilityRNG()
   return set[index];
 }
 
-
 function fakeType(h1e, pe, h1, p)
 {
   if(h1.length != 0)
@@ -122,11 +121,90 @@ function fakeType(h1e, pe, h1, p)
   }
 }
 
+function setup_theme()
+{
+  var light_theme = true;
+  var root = document.documentElement;
+  function toggle_theme()
+  {
+    if(light_theme == true)
+    {
+      this.classList.remove("theme-light");
+      light_theme = false;
+      root.style.setProperty("--bg-color", "#1b1c20");
+      root.style.setProperty("--fg-color", "whitesmoke");
+      root.style.setProperty("--box-color", "rgb(33, 34, 37)");
+      root.style.setProperty("--border-box-color", "black");
+      root.style.setProperty("--link-color", "#008cff");
+    }
+    else
+    {
+      light_theme = true;
+      this.classList.add("theme-light");
+      root.style.setProperty("--bg-color", "white");
+      root.style.setProperty("--fg-color", "black");
+      root.style.setProperty("--box-color", "whitesmoke");
+      root.style.setProperty("--border-box-color", "#cfcfcf");
+      root.style.setProperty("--link-color", "blue");
+    }
+    return;
+  }
+
+  var theme_toggle = document.getElementById("theme-toggle");
+  theme_toggle.addEventListener("click", toggle_theme, false);
+}
+
+function setup_phone_thing()
+{
+  function toggle_view_profile()
+  {
+    var profile = document.querySelector("header");
+    if(profile.classList.contains("no-disp-900"))
+    {
+      profile.classList.remove("no-disp-900");
+    }
+    else
+    {
+      profile.classList.add("no-disp-900");
+    }
+  }
+  var toggle_view_more = document.getElementById("toggle-view-profile");
+  toggle_view_more.addEventListener("click", toggle_view_profile, false);
+  return;
+}
+
+function setup_top_selector()
+{
+  function change_from_top()
+  {
+    var show = document.getElementById(this.dataset.id);
+    var hide = document.querySelector(".display .previous");
+    if(show != hide)
+    {
+      show.classList.remove("no-disp");
+      show.classList.add("previous");
+      
+      hide.classList.add("no-disp");
+      hide.classList.remove("previous");
+    }
+    return;
+  }
+  var selectors = document.getElementsByClassName("top-option");
+  for(var i = 0; i < selectors.length; i++)
+  {
+    selectors[i].addEventListener("click", change_from_top, false);
+  }
+  return;
+}
+
+setup_top_selector();
+setup_phone_thing();
+setup_theme();
 setupTextPopup();
 setupCollapse();
 setupMoreText();
 setupPfpMeme();
-fakeType(document.querySelector(".right h1"), 
-         document.querySelector(".right p"), 
+fakeType(document.querySelector(".js-lol h1"), 
+         document.querySelector(".js-lol p"), 
          "Full_Nitrous", 
          "Honestly, managing memory is better than jerking off.\n\n-anime pic cunt");
